df9ccda05bab7bb245a7c6867fb435c285d64406
Brian Su <briansudev@gmail.com>
2014-05-08 14:38:51 -0700

    first push

---
+++ cal_aps/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for cal_aps project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cal_aps.settings")
+from dj_static import Cling
+from django.core.wsgi import get_wsgi_application
+application = Cling(get_wsgi_application())
